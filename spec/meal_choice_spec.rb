800d7cedf4fc6574fa60ef0a488df534d6a7e176
Heather Kirk <hskirk@gmail.com>
2018-12-24 21:44:01 +0000

    Automatically backed up by Learn

--- spec/meal_choice_spec.rb
+++ spec/meal_choice_spec.rb
@@ -2,11 +2,10 @@ describe '#meal_choice' do
   it 'should default to meat' do
     expect(meal_choice).to eq("meat")
   end
+  def meal_choice(pork)
+end
+puts pork = meat
   it 'should allow you to set a meal' do
     expect(meal_choice("vegan")).to eq("vegan")
   end
-  def meal_choice(meat)
-    meat = meat
-end
-puts meal_choice(meat)
 end
